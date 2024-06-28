340f72c9a39c0c61928bd3697e2d426d9f7a5679
Allyson Rodrigues <125301945+Rodrigues4567@users.noreply.github.com>
2024-06-28 13:52:19 -0300

    Menu responsivo em produção

---
+++ js/main.js
@@ -0,0 +1,30 @@
+
+const button = document.querySelector('.responsive-btn');
+let header = document.querySelector('.header');
+let logoHeader = document.querySelector('.logo-header');
+
+button.addEventListener('click', menuShow = () => {
+    let responsiveList = document.querySelector('.responsive-menu-list');
+
+    if (responsiveList.classList.contains('open')) {
+        responsiveList.classList.remove('open')
+    }
+    else {
+        responsiveList.classList.add('open')
+        // header.style.backgroundColor = '#fff'
+        // header.style.transition = '0.4s'
+        // logoHeader.src = '../img/logo2.png'
+    }
+})
+
+window.addEventListener('scroll', menuScroll = () => {
+    
+    if (window.scrollY > 0) {
+        header.classList.add('roll')
+        logoHeader.src = '../img/logo2.png'
+    }
+    else {
+        header.classList.remove('roll')
+        logoHeader.src = '../img/logo.png'
+    }
+})
